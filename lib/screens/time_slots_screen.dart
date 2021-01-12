3a562e8c081e56d5db0e7cf446e8ee5a2cb9e622
EricSchrappe <e.schrappe@icloud.com>
2021-01-12 18:22:47 +0100

    Added the layout for the time slot administration

---
+++ lib/screens/time_slots_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class ContactDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.2, 0.5, 0.9],
+          colors: [
+            Color(0xFF97FCBE),
+            Color(0xFFb0f6d6),
+            Color(0xFF97CFFC),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  top: 60.0, right: 35.0, left: 35.0, bottom: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    elevation: 5.0,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      child: Icon(
+                        Icons.restore_from_trash,
+                        size: 35.0,
+                        color: Color(0xFF3A6ED4),
+                      ),
+                      radius: 35.0,
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'Please specify the time slots',
+                    style: TextStyle(
+                      color: Color(0xFF444444),
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 25.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Begin date',
+                      style: TextStyle(
+                        color: Color(0xFF444444),
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFF6F6F6),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF3A6ED4),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF3A6ED4),
+                          ),
+                        ),
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(8.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text(
+                      'End date',
+                      style: TextStyle(
+                        color: Color(0xFF444444),
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFF6F6F6),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF3A6ED4),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF3A6ED4),
+                          ),
+                        ),
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(8.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Text(
+                      'Time range',
+                      style: TextStyle(
+                        color: Color(0xFF444444),
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
