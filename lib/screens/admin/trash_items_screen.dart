caa707d88094994f418f342c805c5dfaee8549ca
EricSchrappe <e.schrappe@icloud.com>
2021-02-02 19:00:26 +0100

    created the trash item adjustment page

--- lib/screens/admin/trash_items_screen.dart
+++ lib/screens/admin/trash_items_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:garbagecan/model/item_data.dart';
+import 'package:provider/provider.dart';
 
 class TrashItemsScreen extends StatelessWidget {
   @override
@@ -43,7 +46,7 @@ class TrashItemsScreen extends StatelessWidget {
                     height: 20.0,
                   ),
                   Text(
-                    'Here you can set a new headquarters and adjust the Km ranges',
+                    'Adjust the trash item prices if necessary',
                     style: TextStyle(
                       color: Color(0xFF444444),
                       fontSize: 22.0,
@@ -62,8 +65,94 @@ class TrashItemsScreen extends StatelessWidget {
                   ),
                 ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [],
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.42,
+                      child: Consumer<ItemData>(
+                        builder: (context, itemData, child) {
+                          return ListView.builder(
+                            padding: EdgeInsets.zero,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              final item = itemData.items[index];
+                              return ListTile(
+                                title: Text(
+                                  item.name,
+                                  style: TextStyle(
+                                    color: Color(0xFF444444),
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  'The current value is ${item.value} Rs. per ${item.unit}',
+                                  style: TextStyle(
+                                    color: Color(0xFF444444),
+                                    fontSize: 12.0,
+                                  ),
+                                ),
+                                trailing: SizedBox(
+                                  width: 80.0,
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Expanded(
+                                        flex: 2,
+                                        child: TextField(
+                                          textAlign: TextAlign.center,
+                                          keyboardType: TextInputType.number,
+                                          inputFormatters: [
+                                            FilteringTextInputFormatter
+                                                .digitsOnly
+                                          ],
+                                          decoration: InputDecoration(
+                                            filled: true,
+                                            fillColor: Color(0xFFF6F6F6),
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Color(0xFF3A6ED4),
+                                              ),
+                                            ),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Color(0xFF3A6ED4),
+                                              ),
+                                            ),
+                                            isDense: true,
+                                            contentPadding: EdgeInsets.all(8.0),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 1,
+                                        child: Text(' /${item.unit}'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                            itemCount: itemData.items.length,
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        color: Color(0xFF3A6ED4),
+                        child: Text(
+                          'Adjust items',
+                          style: TextStyle(color: Colors.white, fontSize: 24.0),
+                        ),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
